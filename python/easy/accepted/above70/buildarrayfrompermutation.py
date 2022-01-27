c70748974152034333cfb0a02c1d7f48237d3672
eantablin <eantablin@protonmail.com>
2022-01-27 11:51:35 -0800

    Adds extra rewrite, has it's benefits

--- python/easy/accepted/above70/buildarrayfrompermutation.py
+++ python/easy/accepted/above70/buildarrayfrompermutation.py
@@ -1,14 +1,27 @@
-# Runtime: 120 ms, faster than 76.19% of Python3 online submissions for Build Array from Permutation.
-# Memory Usage: 14.5 MB, less than 40.29% of Python3 online submissions for Build Array from Permutation.
+# Runtime: 236 ms, faster than 6.32% of Python3 online submissions for Build Array from Permutation.
+# Memory Usage: 14.2 MB, less than 99.70% of Python3 online submissions for Build Array from Permutation.
 
 nums = [0,2,1,5,3,4]
 
 def buildArray(nums):
     pHolder = []
     
-    for i in range(len(nums)):
-        pHolder.append(nums[nums[i]])
-    
+    for i in nums:
+        pHolder.append(nums[i])
+        
     return pHolder
+
+# Runtime: 120 ms, faster than 76.19% of Python3 online submissions for Build Array from Permutation.
+# Memory Usage: 14.5 MB, less than 40.29% of Python3 online submissions for Build Array from Permutation.
+
+# nums = [0,2,1,5,3,4]
+
+# def buildArray(nums):
+#     pHolder = []
+    
+#     for i in range(len(nums)):
+#         pHolder.append(nums[nums[i]])
+    
+#     return pHolder
 
 print(buildArray(nums))
